47c8bc10b9e717c0f3ef242370130a3ce5291211
Egor Sonin <logiteclox@gmail.com>
2025-03-28 14:00:20 +0300

    -rate_hw method is now a method of the Reviewer class -Added rate_lecturer method for class Student

--- students_and_mentors.py
+++ students_and_mentors.py
@@ -1,4 +1,5 @@
 class Student:
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -7,28 +8,47 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if 0 < grade <= 10:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return "Ошибка! Оценка должна быть от 0 до 10"
+        else:
+            return "Ошибка"
+
 
 class Mentor:
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 
 class Lecturer(Mentor):
-    pass
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
 
 class Reviewer(Mentor):
-    pass
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if 0 < grade <= 10:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
+            else:
+                return "Ошибка! Оценка должна быть от 0 до 10"
+        else:
+            return "Ошибка"
 
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -42,4 +62,3 @@ class Reviewer(Mentor):
 # cool_mentor.rate_hw(best_student, 'Python', 10)
 #
 # print(best_student.grades)
-
