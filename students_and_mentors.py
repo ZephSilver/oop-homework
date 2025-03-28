f6117cf6e7b7524ad5d8c187357479efc2348467
Egor Sonin <logiteclox@gmail.com>
2025-03-28 16:10:27 +0300

    -Added __str__ method to classes -It is possible to compare objects of classes Student and Lecturer (avg grade)

--- students_and_mentors.py
+++ students_and_mentors.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+@total_ordering
 class Student:
 
     def __init__(self, name, surname, gender):
@@ -7,6 +10,38 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+
+        self_all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        self_avg = sum(self_all_numbers) / len(self_all_numbers) if self_all_numbers else 0
+
+        other_all_numbers = [num for sublist in other.grades.values() for num in sublist]
+        other_avg = sum(other_all_numbers) / len(other_all_numbers) if other_all_numbers else 0
+
+        return self_avg == other_avg
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+
+        self_all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        self_avg = sum(self_all_numbers) / len(self_all_numbers) if self_all_numbers else 0
+
+        other_all_numbers = [num for sublist in other.grades.values() for num in sublist]
+        other_avg = sum(other_all_numbers) / len(other_all_numbers) if other_all_numbers else 0
+
+        return self_avg < other_avg
+
+    def __str__(self):
+        all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        return (f"Имя: {self.name} \n"
+                f"Фамилия: {self.surname} \n"
+                f"Средняя оценка за домашние задания: {round(sum(all_numbers) / len(all_numbers) if all_numbers else 0, 2)} \n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)} \n" 
+                f"Завершенные курсы: {', '.join(self.finished_courses)}")
 
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
@@ -28,15 +63,49 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-
+@total_ordering
 class Lecturer(Mentor):
 
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+
+        self_all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        self_avg = sum(self_all_numbers) / len(self_all_numbers) if self_all_numbers else 0
+
+        other_all_numbers = [num for sublist in other.grades.values() for num in sublist]
+        other_avg = sum(other_all_numbers) / len(other_all_numbers) if other_all_numbers else 0
+
+        return self_avg == other_avg
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+
+        self_all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        self_avg = sum(self_all_numbers) / len(self_all_numbers) if self_all_numbers else 0
+
+        other_all_numbers = [num for sublist in other.grades.values() for num in sublist]
+        other_avg = sum(other_all_numbers) / len(other_all_numbers) if other_all_numbers else 0
+
+        return self_avg < other_avg
+
+    def __str__(self):
+        all_numbers = [num for sublist in self.grades.values() for num in sublist]
+        return (f"Имя: {self.name} \n"
+                f"Фамилия: {self.surname} \n"
+                f"Средняя оценка за лекции: {round(sum(all_numbers) / len(all_numbers) if all_numbers else 0, 2)}")
+
 
 class Reviewer(Mentor):
+
+    def __str__(self):
+        return (f"Имя: {self.name} \n"
+                f"Фамилия: {self.surname}")
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
